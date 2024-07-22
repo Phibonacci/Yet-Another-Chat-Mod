3c889e8a16afc80bc93b8b6824fbb4dca86cd0c3
Jean Fauquenot <git@naej.fr>
2024-07-22 14:53:11 +0200

    Move the bubbles higher so they do not hide the usernames

--- media/lua/client/yacm/utils/coordinates.lua
+++ media/lua/client/yacm/utils/coordinates.lua
@@ -1,51 +1,20 @@
 local coordinates = {}
 
--- function coordinates.CenterTopOfSquare(square, width, height)
---     if square == nil then
---         return nil
---     end
-
---     local zoom = coordinates.GetZoom()
---     local squareWidth = 128 / zoom
---     local squareHeight = 64 / zoom
-
---     local x, y = coordinates.CenterFeetOfPlayer(getPlayer(), width, height)
---     local squareOffsetX = getPlayer():getX() - math.floor(getPlayer():getX())
---     local squareOffsetY = getPlayer():getY() - math.floor(getPlayer():getY())
---     local xOffset = (squareOffsetX - squareOffsetY) * (width / 2)
---     local yOffset = (squareOffsetX + squareOffsetY) * (height / 2) - (height / 2)
-
---     x = x - xOffset
---     y = y - yOffset
-
---     local squareX = math.floor(getPlayer():getX())
-
---     local xTile = j * width / 2 + i * width / 2
---     local yTile = -j * height / 2 + i * height / 2
-
---     -- todo
-
---     local zoom = getCore():getZoom(getPlayer():getPlayerNum())
---     x = x / zoom - width / 2
---     y = y / zoom - height
---     local bodyHeight = 120 / zoom
---     y = y - bodyHeight - 10
---     return x, y
--- end
-
 function coordinates.CenterTopOfPlayer(player, width, height)
     if player == nil then
+        print('CenterTopOfPlayer: nil player parameter')
         return nil
     end
     local x, y = coordinates.CenterTopOfObject(player, width, height)
     local zoom = coordinates.GetZoom()
-    local bodyHeight = 120 / zoom
-    y = y - bodyHeight - 10
+    local bodyHeight = 129 / zoom
+    y = y - bodyHeight - 21
     return x, y
 end
 
 function coordinates.CenterTopOfObject(object, width, height)
     if object == nil then
+        print('CenterTopOfObject: nil player parameter')
         return nil
     end
     local x, y = ISCoordConversion.ToScreen(object:getX(), object:getY(), object:getZ(), nil)
@@ -56,6 +25,10 @@ function coordinates.CenterTopOfObject(object, width, height)
 end
 
 function coordinates.CenterFeetOfPlayer(player, width, height)
+    if player == nil then
+        print('CenterFeetOfPlayer: nil player parameter')
+        return nil
+    end
     local x, y = ISCoordConversion.ToScreen(player:getX(), player:getY(), player:getZ(), nil)
     local zoom = getCore():getZoom(getPlayer():getPlayerNum())
     x = x / zoom - width / 2
