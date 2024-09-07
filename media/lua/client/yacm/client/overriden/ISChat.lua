24bbec0d9d2aa166f7d0b21686b0efd52cb79fd0
Phibonacci <git@naej.fr>
2024-09-07 20:44:38 +0200

    Clean code and remove trailing comma when printing radio frequency in chat

--- media/lua/client/yacm/client/overriden/ISChat.lua
+++ media/lua/client/yacm/client/overriden/ISChat.lua
@@ -562,7 +562,7 @@ function BuildMessageFromPacket(type, message, author, playerColor, frequency)
     local parsedMessage = Parser.ParseYacmMessage(message, messageColor, 20, 200)
     local radioPrefix = ''
     if frequency then
-        radioPrefix = '(' .. string.format('%.1fMHz', frequency / 1000) .. '), '
+        radioPrefix = '(' .. string.format('%.1fMHz', frequency / 1000) .. ') '
     end
     local messageColorString = StringBuilder.BuildBracketColorString(messageColor)
     local quote
@@ -1057,66 +1057,6 @@ ISChat.addLineInChat = function(message, tabID)
         return
     else
         return
-    end
-
-    if not ISChat.instance.chatText then
-        ISChat.instance.chatText = ISChat.instance.defaultTab
-        ISChat.instance:onActivateView()
-    end
-    local chatText
-    if tabID + 1 == 1 then
-        chatText = ISChat.instance.tabs[tabID + 1]
-    elseif tabID + 1 == 2 then
-        chatText = ISChat.instance.tabs[tabID + 1]
-    else
-        print('yacm error: addLineInChat: unknown id ' .. tabID)
-        return
-    end
-    if chatText.tabTitle ~= ISChat.instance.chatText.tabTitle then
-        local alreadyExist = false
-        for i, blinkedTab in ipairs(ISChat.instance.panel.blinkTabs) do
-            if blinkedTab == chatText.tabTitle then
-                alreadyExist = true
-                break
-            end
-        end
-        if alreadyExist == false then
-            table.insert(ISChat.instance.panel.blinkTabs, chatText.tabTitle)
-        end
-    end
-    local vscroll = chatText.vscroll
-    local scrolledToBottom = (chatText:getScrollHeight() <= chatText:getHeight()) or (vscroll and vscroll.pos == 1)
-    if #chatText.chatTextLines > ISChat.maxLine then
-        local newLines = {}
-        for i, v in ipairs(chatText.chatTextLines) do
-            if i ~= 1 then
-                table.insert(newLines, v)
-            end
-        end
-        table.insert(newLines, line .. ' <LINE> ')
-        chatText.chatTextLines = newLines
-    else
-        table.insert(chatText.chatTextLines, line .. ' <LINE> ')
-    end
-    chatText.text = ''
-    local newText = ''
-    for i, v in ipairs(chatText.chatTextLines) do
-        if i == #chatText.chatTextLines then
-            v = string.gsub(v, ' <LINE> $', '')
-        end
-        newText = newText .. v
-    end
-    chatText.text = newText
-    chatText.chatTextRawLines = chatText.chatTextRawLines or {}
-    table.insert(chatText.chatTextRawLines,
-        {
-            time = Calendar.getInstance():getTimeInMillis(),
-            line = message:getText(),
-        })
-    table.insert(chatText.chatMessages, message)
-    chatText:paginate()
-    if scrolledToBottom then
-        chatText:setYScroll(-10000)
     end
 end
 
